bdfe445b7d38c38f6525f2385162c17d78895163
dzakiabrar <dzakiabrar.fi@gmail.com>
2025-09-23 22:13:54 +0700

    tugas 4 done

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+# halaman berbeda
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -11,15 +13,19 @@ class Product(models.Model):
         ('analysis', 'Analysis'),
     ]
     
+    # setiap user memiliki halaman yang berbeda
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1) #data lama dikasih user 1
     #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     thumbnail = models.URLField(blank=True, null=True)
-    #news_views = models.PositiveIntegerField(default=0)
+    news_views = models.PositiveIntegerField(default=0)
     #created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
+    
+
     
     def __str__(self):
         return self.title
@@ -31,3 +37,9 @@ class Product(models.Model):
     def increment_views(self):
         self.news_views += 1
         self.save()
+
+# class Employe(models.Model):
+
+#     Name = models.CharField(max_length=255)
+#     age = models.IntegerField()
+#     persona = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,32 +1,62 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import ProductForm
 from main.models import Product
 
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+# @login_required(login_url='/login')
+# def show_main(request):
+#     # news_list = Product.objects.all()
+#     news_list = Product.objects.filter(user=request.user)  # hanya ambil data milik user login
+
+#     context = {
+#         'npm' : '2306275241',
+#         'name': 'dzaki abrar',
+#         'class': 'PBP C',
+#         'news_list': news_list
+#     }
+
+#     return render(request, "main.html", context)
+
+@login_required(login_url='/login')
 def show_main(request):
-    news_list = Product.objects.all()
+    news_list = Product.objects.filter(user=request.user)
 
     context = {
         'npm' : '2306275241',
         'name': 'dzaki abrar',
         'class': 'PBP C',
-        'news_list': news_list
+        'news_list': news_list,
+        'last_login': request.COOKIES.get('last_login'),  # ambil cookie
     }
 
     return render(request, "main.html", context)
+
 
 def create_news(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
+        # simpan user login
+        product = form.save(commit=False)
+        product.user = request.user   # simpan user yang login
+
         form.save()
         return redirect('main:show_main')
 
     context = {'form': form}
     return render(request, "create_news.html", context)
 
+@login_required(login_url='/login')
 def show_news(request, id):
     news = get_object_or_404(Product, pk=id)
     news.increment_views()
@@ -62,3 +92,68 @@ def show_json_by_id(request, news_id):
        return HttpResponse(json_data, content_type="application/json")
    except Product.DoesNotExist:
        return HttpResponse(status=404)
+   
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+# def login_user(request):
+#    if request.method == 'POST':
+#       form = AuthenticationForm(data=request.POST)
+
+#       if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('main:show_main')
+
+#    else:
+#       form = AuthenticationForm(request)
+#    context = {'form': form}
+#    return render(request, 'login.html', context)
+
+#menerapkan cookie last login
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            # bikin response redirect
+            response = HttpResponseRedirect('/')
+
+            # set cookie last_login
+            response.set_cookie('last_login', str(timezone.now()))
+
+            return response
+    else:
+        form = AuthenticationForm(request)
+
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+# def logout_user(request):
+#     logout(request)
+#     return redirect('main:login')
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
+# def addEmploye(request):
+
+#     newlist =  Employe.objects.all()
+
+#     return request
